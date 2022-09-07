19fbb071695240ecbc35038f824c4fdeecb65c32
fqamic <fqamic@vip.qq.com>
2022-09-07 19:50:02 +0800

    Revert "feat: support xr config"
    This reverts commit 0cef02c496660de09e28b643945fa966bdacc2d3.

--- native/cocos/bindings/auto/jsb_xr_auto.h
+++ native/cocos/bindings/auto/jsb_xr_auto.h
@@ -9,27 +9,8 @@
 
 bool register_all_xr(se::Object *obj);                   // NOLINT
 
-JSB_REGISTER_OBJECT_TYPE(cc::xr::XRConfigValue);
 JSB_REGISTER_OBJECT_TYPE(cc::xr::XrEntry);
 
-
-extern se::Object *__jsb_cc_xr_XRConfigValue_proto;   // NOLINT
-extern se::Class *__jsb_cc_xr_XRConfigValue_class;    // NOLINT
-
-bool js_register_cc_xr_XRConfigValue(se::Object *obj); // NOLINT
-
-template <>
-bool sevalue_to_native(const se::Value &, cc::xr::XRConfigValue *, se::Object *ctx); //NOLINT
-SE_DECLARE_FUNC(js_xr_XRConfigValue_getBool);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_getFloat);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_getInt);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_getPointer);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_getString);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_isBool);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_isFloat);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_isInt);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_isPointer);
-SE_DECLARE_FUNC(js_xr_XRConfigValue_isString);
 
 extern se::Object *__jsb_cc_xr_XrEntry_proto;   // NOLINT
 extern se::Class *__jsb_cc_xr_XrEntry_class;    // NOLINT
@@ -47,7 +28,6 @@ SE_DECLARE_FUNC(js_xr_XrEntry_getCurrentXrSwapchain);
 SE_DECLARE_FUNC(js_xr_XrEntry_getHMDViewPosition);
 SE_DECLARE_FUNC(js_xr_XrEntry_getSwapchainImageIndex);
 SE_DECLARE_FUNC(js_xr_XrEntry_getXRBoolConfig);
-SE_DECLARE_FUNC(js_xr_XrEntry_getXRConfig);
 SE_DECLARE_FUNC(js_xr_XrEntry_getXRFloatConfig);
 SE_DECLARE_FUNC(js_xr_XrEntry_getXRIntConfig);
 SE_DECLARE_FUNC(js_xr_XrEntry_getXRPointerConfig);
@@ -69,8 +49,6 @@ SE_DECLARE_FUNC(js_xr_XrEntry_setIPDOffset);
 SE_DECLARE_FUNC(js_xr_XrEntry_setMultisamplesRTT);
 SE_DECLARE_FUNC(js_xr_XrEntry_setRenderingScale);
 SE_DECLARE_FUNC(js_xr_XrEntry_setXRBoolConfig);
-SE_DECLARE_FUNC(js_xr_XrEntry_setXRConfig);
-SE_DECLARE_FUNC(js_xr_XrEntry_setXRConfigCallback);
 SE_DECLARE_FUNC(js_xr_XrEntry_setXRFloatConfig);
 SE_DECLARE_FUNC(js_xr_XrEntry_setXRIntConfig);
 SE_DECLARE_FUNC(js_xr_XrEntry_setXRPointerConfig);
