b7544a1f64f32c2b20272a0eca3d449d15d46c1c
fqamic <fqamic@vip.qq.com>
2022-10-10 16:40:04 +0800

    feat: add xr environment constant

--- cocos/ar/index.ts
+++ cocos/ar/index.ts
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 Xiamen Yaji Software Co., Ltd.
+Copyright (c) 2022 Xiamen Yaji Software Co., Ltd.
 
 https://www.cocos.com/
 
@@ -22,10 +22,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-
-/**
- * @hidden
- */
 
 export * from './ar-features';
 export * from './ar-feature-base';
