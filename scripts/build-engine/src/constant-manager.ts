a41832cf65a48ae8e1ed3a92a66f0550fbdd2629
fqamic <fqamic@vip.qq.com>
2022-10-11 16:51:05 +0800

    improve: open xr environment to developer

--- scripts/build-engine/src/constant-manager.ts
+++ scripts/build-engine/src/constant-manager.ts
@@ -6,8 +6,8 @@ export type ModeType = 'EDITOR' | 'PREVIEW' | 'BUILD' | 'TEST';
 export type PlatformType = 'HTML5' | 'NATIVE' |
         'WECHAT' | 'BAIDU' | 'XIAOMI' | 'ALIPAY' | 'BYTEDANCE' |
         'OPPO' | 'VIVO' | 'HUAWEI' | 'COCOSPLAY' | 'QTT' | 'LINKSURE';
-export type InternalFlagType = 'SERVER_MODE' | 'NOT_PACK_PHYSX_LIBS' | 'WEBGPU' | 'XR_ENV' | 'XR_SWITCH';
-export type PublicFlagType = 'DEBUG' | 'NET_MODE';
+export type InternalFlagType = 'SERVER_MODE' | 'NOT_PACK_PHYSX_LIBS' | 'WEBGPU';
+export type PublicFlagType = 'DEBUG' | 'NET_MODE' | 'XR_ENV' | 'XR_TYPE';
 export type FlagType = InternalFlagType | PublicFlagType;
 
 export type ValueType = number | boolean;
