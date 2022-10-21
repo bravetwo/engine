f21f6f0c7788be930f31cc9343737d2e33a1cfce
oahc09 <oahc09@gmail.com>
2022-10-21 19:05:57 +0800

    feat:add nativeTexturePtr api to get texture id in gles

--- native/cocos/renderer/gfx-validator/TextureValidator.h
+++ native/cocos/renderer/gfx-validator/TextureValidator.h
@@ -43,6 +43,8 @@ public:
 
     const Texture *getRaw() const override { return _actor->getRaw(); }
 
+    uint32_t getNativeTexturePtr() const override { return _actor->getNativeTexturePtr(); }
+
 protected:
     friend class SwapchainValidator;
 
